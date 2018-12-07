ecdbc97080b3c8b51ca106b5c7916c38c56f3b1a
Szymon Darmofał <darmofal.szymon@gmail.com>
2018-12-07 10:26:00 +0100

    More tests for commands

--- application/test_commands.py
+++ application/test_commands.py
@@ -1,10 +1,4 @@
-# def test_will_handle_ping_command():
-#   bus = CommandBus()
-#   light = Light()
-#   command = LightOnCommand(light)
-#   result = bus.execute(command)
-#   assert result.status == CommandResultStatus.OK
-
+from schematics.exceptions import DataError
 
 from application.commands import AddItemCommand
 
@@ -18,6 +12,15 @@ def test_add_item_command_title_is_required():
     command = AddItemCommand({'seller_id': 'some_id_here', 'description': 'Fluffy dragon'})
     assert command.is_valid() is False
 
-# def test_add_item_command_will_return_errors():
-#     command = AddItemCommand({ 'description': 'Fluffy dragon' })
-#     assert command.get_validation_errors() == False
+
+def test_add_item_command_will_return_errors():
+    command = AddItemCommand({'description': 'Fluffy dragon'})
+    actual: DataError = command.validation_errors()
+    assert actual.errors['seller_id'] is not None
+    assert actual.errors['title'] is not None
+
+
+def test_add_item_command_will_not_return_errors_when_command_is_valid():
+    command = AddItemCommand({'seller_id': 'some_id_here', 'title': 'Fluffy dragon', 'description': 'Fluffy dragon'})
+    actual: DataError = command.validation_errors()
+    assert actual is None

4fa7f1e9cfb421e734b4e0733a33d406ee12ba5e
Szymon Darmofał <darmofal.szymon@gmail.com>
2018-12-07 10:02:03 +0100

    Missing tests

--- application/test_commands.py
+++ application/test_commands.py
@@ -1,5 +1,3 @@
-
-
 # def test_will_handle_ping_command():
 #   bus = CommandBus()
 #   light = Light()
@@ -8,16 +6,17 @@
 #   assert result.status == CommandResultStatus.OK
 
 
-
 from application.commands import AddItemCommand
 
+
 def test_valid_add_item_command():
-    command = AddItemCommand({ 'title': 'Fluffy dragon' })
-    assert command.is_valid() == True
+    command = AddItemCommand({'seller_id': 'some_id_here', 'title': 'Fluffy dragon'})
+    assert command.is_valid() is True
+
 
 def test_add_item_command_title_is_required():
-    command = AddItemCommand({ 'description': 'Fluffy dragon' })
-    assert command.is_valid() == False
+    command = AddItemCommand({'seller_id': 'some_id_here', 'description': 'Fluffy dragon'})
+    assert command.is_valid() is False
 
 # def test_add_item_command_will_return_errors():
 #     command = AddItemCommand({ 'description': 'Fluffy dragon' })
